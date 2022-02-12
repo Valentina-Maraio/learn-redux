5e12b2b0059d1ea63a6e48040848bcc0d6504af9
Valentina Maraio <valentina.maraio89@gmail.com>
2022-02-12 13:40:29 +0100

    mahh

--- redux/src/comps/header/Header.js
+++ redux/src/comps/header/Header.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
       <div>
-        <Nav className="justify-content-end" activeKey="/home">
-            <Nav.Item>
-                <Nav.Link href="/home">Favoriti</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link eventKey="link-1">Shopping List</Nav.Link>
-            </Nav.Item>
-        </Nav>
+        
       </div>
   )
 }
